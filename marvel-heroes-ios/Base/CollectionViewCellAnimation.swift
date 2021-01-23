47e964ae4404e7b8815c065058b088eed7377bff
Anil <anilakkaya58@gmail.com>
2021-01-24 00:08:42 +0300

    added cool animation extension!

--- marvel-heroes-ios/Base/CollectionViewCellAnimation.swift
+++ marvel-heroes-ios/Base/CollectionViewCellAnimation.swift
@@ -5,4 +5,17 @@
 //  Created by Josh on 23.01.2021.
 //
 
-import Foundation
+import UIKit
+
+
+extension UICollectionViewCell {
+    func animateCell() {
+        self.alpha = 0
+        let transform = CATransform3DTranslate(CATransform3DIdentity, -250, 0, 0)
+        self.layer.transform = transform
+        UIView.animate(withDuration: 1, delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 0.5, options: .curveEaseOut, animations: {
+            self.alpha = 1
+            self.layer.transform = CATransform3DIdentity
+        }, completion: nil)
+    }
+}
