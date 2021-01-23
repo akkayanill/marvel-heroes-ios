50a3df77d27377595a18138c756a8370924ce690
Anil <anilakkaya58@gmail.com>
2021-01-24 00:12:39 +0300

    working on HeroDetail Screen's UI

--- marvel-heroes-ios/HeroDetail/Views/HeroDetailHeaderCell.swift
+++ marvel-heroes-ios/HeroDetail/Views/HeroDetailHeaderCell.swift
@@ -6,3 +6,86 @@
 //
 
 import Foundation
+import UIKit
+
+
+class HeroDetailHeaderCell: UICollectionViewCell {
+    
+    public var animator: UIViewPropertyAnimator?
+    
+    let imageView: UIImageView = {
+        let imgView = UIImageView()
+        imgView.translatesAutoresizingMaskIntoConstraints = false
+        imgView.contentMode = .scaleAspectFill
+        imgView.backgroundColor = .clear
+        
+        return imgView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        backgroundColor = .white
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    private func layoutViews() {
+        addSubview(imageView)
+        imageView.fillToSuperview()
+    }
+    
+    // MARK:- add visual effect view
+    private func addBlurVisulaEffectView() {
+        
+        animator = UIViewPropertyAnimator(duration: 4.0, curve: .easeInOut, animations: { [weak self] in
+            guard let strongSelf = self else { return }
+            
+            let blurEffect = UIBlurEffect(style: .regular)
+            let visulaEffectView = UIVisualEffectView(effect: blurEffect)
+            visulaEffectView.translatesAutoresizingMaskIntoConstraints = false
+            strongSelf.addSubview(visulaEffectView)
+            visulaEffectView.fillToSuperview()
+//            NSLayoutConstraint.activate([
+//                visulaEffectView.topAnchor.constraint(equalTo: strongSelf.topAnchor, constant: 0),
+//                visulaEffectView.leadingAnchor.constraint(equalTo: strongSelf.leadingAnchor, constant: 0),
+//                visulaEffectView.bottomAnchor.constraint(equalTo: strongSelf.bottomAnchor, constant: 0),
+//                visulaEffectView.trailingAnchor.constraint(equalTo: strongSelf.trailingAnchor, constant: 0)
+//            ])
+        })
+    }
+}
+
+
+
+//MARK: - CustomHeaderLayout
+class CustomHeaderLayout: UICollectionViewFlowLayout {
+    
+    override func layoutAttributesForElements(in rect: CGRect) -> [UICollectionViewLayoutAttributes]? {
+        let layoutAttributes = super.layoutAttributesForElements(in: rect)
+        
+        layoutAttributes?.forEach({ (attribute) in
+            if attribute.representedElementKind == UICollectionView.elementKindSectionHeader {
+                
+                guard let collectionView = collectionView else { return }
+                let contentOffsetY = collectionView.contentOffset.y
+                
+                if contentOffsetY < 0 {
+                    let width = collectionView.frame.width
+                    
+                    let height = attribute.frame.height - contentOffsetY
+                    attribute.frame = CGRect(x: 0, y: contentOffsetY, width: width, height: height)
+                }
+            }
+        })
+        return layoutAttributes
+    }
+    
+    override func shouldInvalidateLayout(forBoundsChange newBounds: CGRect) -> Bool {
+        return true
+    }
+}
--- marvel-heroes-ios/HeroDetail/Views/HeroDetailViewController.swift
+++ marvel-heroes-ios/HeroDetail/Views/HeroDetailViewController.swift
@@ -8,22 +8,107 @@
 import Foundation
 import UIKit
 
+
 class HeroDetailViewController: BaseViewController {
     
     
+    
     //MARK: - Variable & Constants
+    var cellSize: CGFloat {
+        return screenSize.width/2.0 - 30.0
+    }
+    
+    let viewModel = HeroDetailViewModel()
+    
+    var header: HeroDetailHeaderCell?
+    
+    
+    
+    //MARK: - Visual Objects
+    let collectionView: UICollectionView = {
+        let layout = CustomHeaderLayout()
+        layout.scrollDirection = .vertical
+        
+        let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        cv.translatesAutoresizingMaskIntoConstraints = false
+        cv.contentOffset = .zero
+        cv.showsVerticalScrollIndicator = false
+        cv.backgroundColor = .white
+        
+        return cv
+    }()
     
     
     
     //MARK: - View Appearence
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        title = viewModel.character.name ?? "Unknown Name"
+        setupCollectionView()
+        addBackButton()
+    }
+    
+    private func setupCollectionView() {
+        collectionView.register(supplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withClass: HeroDetailHeaderCell.self)
+        collectionView.register(cellWithClass: HeroCollectionViewCell.self)
+        collectionView.delegate = self
+        collectionView.dataSource = self
+    }
+    
+    
+    
+    //MARK: - ScrollView
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let contentOffsetY = scrollView.contentOffset.y
+        print(contentOffsetY)
+        if contentOffsetY < -40 {
+            header?.animator?.fractionComplete = abs(contentOffsetY) / 200
+        } else {
+            header?.animator?.fractionComplete = 0.0
+        }
     }
     
     
     
     //MARK: - Layout
     override func layoutViews() {
+        view.addSubview(collectionView)
+        collectionView.fillToSuperview()
+    }
+}
+
+
+
+//MARK: - Collection View Data Source & Delegate
+extension HeroDetailViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withClass: HeroCollectionViewCell.self, for: indexPath)
         
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 20
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 25, left: 0, bottom: 20, right: 0)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        self.header = collectionView.dequeueReusableSupplementaryView(ofKind: UICollectionView.elementKindSectionHeader, withClass: HeroDetailHeaderCell.self, for: indexPath)
+        return self.header!
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        return CGSize(width: screenSize.width, height: 450)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: screenSize.width, height: cellSize+24)
     }
 }
