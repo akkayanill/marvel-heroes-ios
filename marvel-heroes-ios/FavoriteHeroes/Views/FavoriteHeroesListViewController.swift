67db9ddf9ab46561dd11ecb854f82f0293f2b174
Anil <anilakkaya58@gmail.com>
2021-01-25 00:26:29 +0300

    created basic files for FavoriteHeroesList

---
+++ marvel-heroes-ios/FavoriteHeroes/Views/FavoriteHeroesListViewController.swift
@@ -0,0 +1,14 @@
+//
+//  FavoriteHeroesListViewController.swift
+//  marvel-heroes-ios
+//
+//  Created by Josh on 25.01.2021.
+//
+
+import Foundation
+import UIKit
+
+
+class FavoriteHeroesListViewController: BaseViewController {
+    
+}
